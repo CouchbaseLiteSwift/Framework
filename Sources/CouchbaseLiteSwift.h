e2986e04abac99df50f7e8505c17ffd9089d2454
Marco Betschart <marco@mandelkind.swiss>
2016-04-17 23:46:21 +0200

    Removed "Copy CouchbaseLite Build Phase"

--- Sources/CouchbaseLiteSwift.h
+++ Sources/CouchbaseLiteSwift.h
@@ -6,14 +6,12 @@
 //  Copyright © 2016 MANDELKIND. All rights reserved.
 //
 
-//#import <Foundation/Foundation.h>
-//
-////! Project version number for CouchbaseLiteSwift.
-//FOUNDATION_EXPORT double CouchbaseLiteSwiftVersionNumber;
-//
-////! Project version string for CouchbaseLiteSwift.
-//FOUNDATION_EXPORT const unsigned char CouchbaseLiteSwiftVersionString[];
-//
-//// In this header, you should import all the public headers of your framework using statements like #import <CouchbaseLiteSwift/PublicHeader.h>
-//
-//
+#import <Foundation/Foundation.h>
+
+//! Project version number for CouchbaseLiteSwift.
+FOUNDATION_EXPORT double CouchbaseLiteSwiftVersionNumber;
+
+//! Project version string for CouchbaseLiteSwift.
+FOUNDATION_EXPORT const unsigned char CouchbaseLiteSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CouchbaseLiteSwift/PublicHeader.h>
