8d03f991c7325cf4d9df2954f8b0219602548b7a
Marco Betschart <marco@mandelkind.swiss>
2016-04-21 00:23:45 +0200

    CouchbaseLiteSwift Header

---
+++ Sources/CouchbaseLiteSwift.h
@@ -0,0 +1,17 @@
+//
+//  CouchbaseLiteSwift.h
+//  CouchbaseLiteSwift
+//
+//  Created by Marco Betschart on 21.04.16.
+//  Copyright © 2016 MANDELKIND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CouchbaseLiteSwift.
+FOUNDATION_EXPORT double CouchbaseLiteSwiftVersionNumber;
+
+//! Project version string for CouchbaseLiteSwift.
+FOUNDATION_EXPORT const unsigned char CouchbaseLiteSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CouchbaseLiteSwift/PublicHeader.h>
